b63373d5d89c82ae277a04905846746e1d35a41b
HEROgold <martijnwieringa28@gmail.com>
2024-04-28 22:14:43 +0200

    fix config

--- config.py
+++ config.py
@@ -1,19 +1,26 @@
 # noqa: D100
 
+import secrets
 from pathlib import Path
-from tomllib import loads
+from tomllib import load
 
 
-toml = loads("pyproject.toml")
+TOML_FILE = Path(__file__).parent / "pyproject.toml"
+
+with TOML_FILE.open("rb") as file:
+    toml = load(file)
+
 
 PROJECT_NAME = toml["project"]["name"]
 PROJECT_VERSION = toml["project"]["version"]
+SECRET_KEY = secrets.token_hex(64)
 SERVERS_DIRECTORY = Path(__file__).parent / "servers"
 DOWNLOADS_DIRECTORY = Path(__file__).parent / "downloads"
 DATABASE_PATH = Path(__file__).parent / "database.db"
 
 MODS_API_URL = "https://mods.factorio.com/api/mods"
 LOGIN_URL = "https://www.factorio.com/login"
+LOGIN_API = "https://auth.factorio.com/api-login"
 RELEASES_URL = "https://factorio.com/api/latest-releases"
 ARCHIVE_URL = "https://www.factorio.com/download/archive"
 AVAILABLE_UPDATES_URL = "https://updater.factorio.com/get-available-versions."
