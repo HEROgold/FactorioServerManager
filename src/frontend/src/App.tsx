b491f72c41c6fcb4362a163bcd2ce3a68b731ff4
HEROgold <martijnwieringa28@gmail.com>
2025-01-26 01:37:56 +0100

    add page content

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -3,12 +3,16 @@ import './static/css/main.css';
 import Footer from './components/footer.tsx';
 import Header from './components/header.tsx';
 import Navbar from './components/navbar.tsx';
+import Login from './routes/login.tsx';
+import HomePage from './routes/home.tsx';
 
 function App() {
   return (
       <div className="App">
         <Header />
         <Navbar isAuthenticated={undefined} displayName={undefined} />
+        <HomePage />
+        <Login />
         <Footer />
       </div>
   );
