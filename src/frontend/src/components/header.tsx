b10c7c50e94dbb0b820e20919cda88aa55c4238c
HEROgold <martijnwieringa28@gmail.com>
2025-01-30 21:19:35 +0100

    format

--- src/frontend/src/components/header.tsx
+++ src/frontend/src/components/header.tsx
@@ -4,9 +4,9 @@ function Header() {
     return (
         <header className="App-header">
             <div class="header-inner">
-            <a href="https://factorio.com" class="header-logo">
-            <img src="https://cdn.factorio.com/assets/img/web/factorio-logo.png" alt="Factorio Logo"></img>
-            </a>
+                <a href="https://factorio.com" class="header-logo">
+                    <img src="https://cdn.factorio.com/assets/img/web/factorio-logo.png" alt="Factorio Logo"></img>
+                </a>
             </div>
         </header>
     );
