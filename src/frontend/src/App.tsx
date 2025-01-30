164e7efd273b2ae22910c6a109813a7e3a6bf380
HEROgold <martijnwieringa28@gmail.com>
2025-01-30 21:19:54 +0100

    match signature

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
       <div className="App">
         <Header />
-        <Navbar isAuthenticated={undefined} displayName={undefined} />
+        <Navbar />
         <Routing />
         <Footer />
       </div>
