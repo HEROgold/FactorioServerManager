07d3e328fc5bef1f1ee5a86320581b94793dfe44
HEROgold <martijnwieringa28@gmail.com>
2025-02-07 15:25:49 +0100

    fix running program async

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,7 +1,12 @@
 """Run the FastAPI app."""
-import uvicorn
-from app import app
+import asyncio
 
+import uvicorn
+
+
+async def main() -> None:
+    """Run the FastAPI app."""
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)  # noqa: S104
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)  # noqa: S104
+    asyncio.run(main())
