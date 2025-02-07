8f48ada424785e34e435992640713db5c6201e93
HEROgold <martijnwieringa28@gmail.com>
2025-02-07 15:50:57 +0100

    add todo

--- backend/api/server.py
+++ backend/api/server.py
@@ -1,9 +1,28 @@
 """Server API endpoints."""
-from fastapi import APIRouter
+
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+
+
+# TODO @HEROgold: move this class to a separate file
+# 0
+class User:
+    """Class Representing a User."""
+    async def get_servers(self) -> list[str]:
+        """Get the servers that the user has access to."""
+        return ["server1", "server2"]
+
+
+async def get_current_user() -> User:
+    """Get the current user."""
+    return User()
 
 
 router = APIRouter(prefix="/server", tags=["server"])
 
+
 @router.get("/list")
-async def list_servers(user):
-    return {"servers": []}
+async def list_servers(user: Annotated[User, Depends(get_current_user)]) -> list[str]:
+    """List all servers that the user has access to."""
+    return await user.get_servers()
